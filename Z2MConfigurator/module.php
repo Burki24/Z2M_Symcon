d1e90ac940ee6835b125594e96ff9197affae445
Burki24 <burkhard@kneiseler.de>
2024-02-04 12:03:16 +0100

    FIX: Configurator

--- Z2MConfigurator/module.php
+++ Z2MConfigurator/module.php
@@ -3,7 +3,7 @@
 declare(strict_types=1);
 require_once __DIR__ . '/../libs/MQTTHelper.php';
 
-class Zigbee2MQTTConfigurator extends IPSModule
+class Z2MConfigurator extends IPSModule
 {
     use \Z2MSymcon\MQTTHelper;
 
