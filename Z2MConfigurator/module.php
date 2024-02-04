7332531d296728a94ce9ee2786a849eb2efa4b0a
Burki24 <burkhard@kneiseler.de>
2024-02-04 12:14:28 +0100

    FIX: Configurator

--- Z2MConfigurator/module.php
+++ Z2MConfigurator/module.php
@@ -5,7 +5,7 @@ require_once __DIR__ . '/../libs/MQTTHelper.php';
 
 class Z2MConfigurator extends IPSModule
 {
-    use \Symcon\libs\MQTTHelper;
+    use \Z2M_Symcon\MQTTHelper;
 
     public function Create()
     {
